3a1587c02e0c4e99c6ed8d4dc44ab35fb38d3a9d
Melvin Tebalan <mtebaland@miumg.edu.gt>
2024-09-07 22:27:15 -0600

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   environment: "local",
-  api: "https://localhost:7082/api/",
+  //api: "https://localhost:7082/api/",
+  api: "http://sgi.somee.com/api/",
   filenameCsv: "dev-pos"
 };
